73023dbf3964a81b900972f2255eae0bc570cb68
Emmanuel Sales <absolutezero2a03@gmail.com>
2020-04-13 17:40:10 -0700

    more test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,14 @@ using Circuits
     connect_nodes!(c, "n1", "n2", Resistor(5.0))
     connect_nodes!(c, "n2", "GND", Resistor(10.0))
     println(solve_voltages(c))
+
+    c2 = Circuit()
+    register_node!(c2, "n1", VoltageSource(12.0))
+    register_node!(c2, "n2", BaseNode())
+    register_node!(c2, "n3", BaseNode())
+    connect_nodes!(c2, "n1", "n2", Resistor(4000.))
+    connect_nodes!(c2, "n2", "n3", Resistor(2000.))
+    connect_nodes!(c2, "n2", "GND", Resistor(1000.))
+    connect_nodes!(c2, "n3", "GND", Resistor(2000.))
+    println(solve_voltages(c2))
 end
